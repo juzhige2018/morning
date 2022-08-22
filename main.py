42512e826f0b8b6d244c010b353113a831615a24
Novartis <84281705+juzhige2018@users.noreply.github.com>
2022-08-22 17:15:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -49,6 +49,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"words":{"value":get_words(), "color":get_random_color()}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"woreds":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
