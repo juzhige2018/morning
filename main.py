7515865b5a0cce98489b3dc11b981b5fd50e8b9f
Novartis <84281705+juzhige2018@users.noreply.github.com>
2022-08-31 16:25:39 +0800

    Update main.py
    更改测试

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea },"temperature":{"value":temperature + "%"},"woreds":{"value":get_words(), "color":get_random_color()}}
+wea, temperature = get_weather() + “%”
+data = {"weather":{"value":wea },"temperature":{"value":temperature},"woreds":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
